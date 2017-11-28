0447687ebdb6ba1890d4cc4ad3c3a869c22e064c
paakcmd <paakcmd@users.noreply.github.com>
2017-11-28 23:14:01 +0700

    new master

---
+++ Rocket Boost/Assets/DataFiles/Scripts/AutoWalk2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AutoWalk2 : MonoBehaviour {
+	public int speed = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+	}
+
+	public void OnTriggerEnter(Collider collide)
+	{
+		if (collide.gameObject.CompareTag ("TrophyTag")) {
+			SceneManager.LoadScene("EndScene");
+		}
+	}
+}
