125a53bd6d18a8471505ed6552d935d91115e789
mkdesilva <mihindu.desilva@gmail.com>
2017-12-10 17:11:14 +0700

    Changed end scene and changed look of Maze2

---
+++ Rocket Boost/Assets/DataFiles/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNextLevel() {
+		if (SceneManager.GetActiveScene ().name == "EndScene") {
+			SceneManager.LoadScene (1);
+		} else {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		}
+	}
+}
